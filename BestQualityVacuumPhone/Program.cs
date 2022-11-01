b526ab32ce29f157ae7fda7f67ea42bd788cf86c
falco <falco@DESKTOP-9BUR4S7>
2022-11-01 03:56:10 +0300

    Add OXT translation

--- BestQualityVacuumPhone/Program.cs
+++ BestQualityVacuumPhone/Program.cs
@@ -3,22 +3,90 @@ using System.Text;
 
 Console.OutputEncoding = Encoding.Unicode;
 
-string text = "Allows you to turn off remote control. In order to remote control this Time Machine step out of the car and open the Interaction Menu.";
-int steps = 75;
+// Configuration
 
-VacuumResult result = await VacuumTranslator.Translate(
-    text,
-    "English",
-    "English",
-    steps: steps,
-    onProgress: (step, from, to, text) =>
+int steps = 5;
+int batch = 20;
+
+string oxtPath = @"C:\\Users\\falco\\Desktop\\global.oxt.txt";
+string outPath = @"C:\\Users\\falco\\Desktop\\lost.oxt.txt";
+
+// End
+
+IEnumerable<string> oxtLines = File
+    .ReadAllLines(oxtPath)
+    .Skip(2)
+    .TakeWhile(x => x != "}");
+
+static void WriteOn(int x, int y, string text)
+{
+    Console.SetCursorPosition(y, x);
+    Console.WriteLine(new string(' ', 100));
+    Console.SetCursorPosition(y, x);
+    Console.Write(text);
+}
+
+Dictionary<string, string> translation = new();
+
+List<string> keys = new();
+StringBuilder batchSb = new();
+
+int index = 0;
+foreach (string oxtLine in oxtLines)
+{
+    string[] parts = oxtLine.Split('=', StringSplitOptions.TrimEntries);
+
+    string key = parts[0];
+    string value = parts[1];
+
+    // Packing batch
+    if (keys.Count < batch)
     {
-        Console.WriteLine(
-            $"{step}/{steps} {from}-{to}");
-    },
-    onError: (attemptsLeft) =>
+        batchSb.AppendLine(value);
+        keys.Add(key);
+        continue;
+    }
+
+    // Sending batch
+    string batchText = batchSb.ToString();
+    VacuumResult result = await VacuumTranslator.Translate(
+        batchText,
+        "English",
+        "English",
+        steps: steps,
+        onProgress: (step, from, to, text) =>
+        {
+            WriteOn(index, 0, $"[{index}] - {((double)step / steps):0%} {step}/{steps} {from}-{to}");
+        },
+        onError: (attemptsLeft) =>
+        {
+            WriteOn(index, 0, $"[{index}] - Connection failed. Attempts left: {attemptsLeft}");
+        });
+
+    // Unpacking batch
+    string[] results = result.Text.Split("\r\n");
+    for (int i = 0; i < results.Length; i++)
     {
-        Console.WriteLine(
-            $"Failed to connect server... Attempts left: {attemptsLeft}");
-    });
-Console.WriteLine(result.Text);
+        WriteOn(index, 0, $"[{index}] - {results[i]}");
+        translation[keys[i]] = results[i];
+        index++;
+    }
+
+    // Cleaning up batch
+    keys.Clear();
+    batchSb.Clear();
+}
+Console.WriteLine($"\n\nWriting {outPath}");
+
+StringBuilder sb = new();
+sb.AppendLine("Version 2 30");
+sb.AppendLine("{");
+foreach (var entry in translation)
+{
+    sb.AppendLine($"\t{entry.Key} = {entry.Value}");
+}
+sb.AppendLine("}");
+
+File.WriteAllText(outPath, sb.ToString());
+
+Console.WriteLine("We hope you enjoyed Best Quality Vacuum Phone OXT Translation services!");
